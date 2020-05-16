a6ca4a29cf9f3082b2c74f5d3640c78407a728c8
Baumdiggity <atomnomicbomb@gmail.com>
2020-05-16 13:09:57 -0700

    WIP Card Deck Building

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Card : MonoBehaviour
 {
     public enum TypesOfCards {Action, Build}; //represents a card type
-    public TypesOfCards myType; 
+    public TypesOfCards myType;
+    public GameObject placedPiece;
     //sprite or image for the card
     public bool played;
     private bool discarded;
@@ -18,10 +21,28 @@ public class Card : MonoBehaviour
 
     public void Play()
     {
-        Debug.Log(myName + " card got played");
-        played = true;
-        gameObject.SetActive(false);
-        //should remove this card from the player hand, now that it has been used?
+        if (!played)
+        {
+
+            switch (myName)
+            {
+                case "PorchFlower":
+                    if (placedPiece != null)
+                    {
+                        GameObject wep = FindObjectOfType<WeaponCollection>().gameObject;
+                        Instantiate(placedPiece, wep.transform);
+                    }
+                    return;
+
+                case null:
+                    Debug.Log("Invalid card type");
+                    return;
+            }
+            Debug.Log(myName + " card got played");
+            played = true;
+            gameObject.SetActive(false);
+            //should remove this card from the player hand, now that it has been used?
+        }
 
     }
 
