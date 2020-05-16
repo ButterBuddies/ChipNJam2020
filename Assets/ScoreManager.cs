227019b5803c95381eadb1de2d0b2f625d964903
ranyo7 <65369571+ranyo7@users.noreply.github.com>
2020-05-16 14:22:28 -0500

    Score Manager, Fixed enemy colldiers

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    [SerializeField] Text scoreText;
+    int score = 0;
+
+    private void Start()
+    {
+        UpdateScore();
+    }
+
+    public void IncrementScore(int amount)
+    {
+        score += amount;
+        UpdateScore();
+    }
+
+    public void DecrementScore(int amount)
+    {
+        if ((score -= amount) < 0)
+            score = 0;
+        UpdateScore();
+    }
+
+    public void UpdateScore()
+    {
+        scoreText.text = score.ToString();
+    }
+
+}
