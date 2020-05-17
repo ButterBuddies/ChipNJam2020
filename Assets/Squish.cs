adf971d4c96cfc2c0f975fb49dffaea1f6f84b3e
sarahcrichmond <sarah.richmond@medcognition.com>
2020-05-17 10:22:42 -0500

    Made beetle squishable

--- Assets/Squish.cs
+++ Assets/Squish.cs
@@ -6,7 +6,7 @@ public class Squish : MonoBehaviour
 {
     public GameObject[] squishGraphics;
 
-    public void Squished()
+    public void SquishedDeath()
     {
         GameObject pickedGraphic = squishGraphics[Random.Range(0, 2)];
         pickedGraphic.SetActive(true);
@@ -14,5 +14,21 @@ public class Squish : MonoBehaviour
         m.PlayOneShot(m.clip);
         pickedGraphic.transform.SetParent(null);
         gameObject.SetActive(false);
+    }
+
+    public void SquishDamage()
+    {
+
+        GetComponent<EnemyMovement>().health -= 5;
+        if (GetComponent<EnemyMovement>().health <= 0)
+        {
+            SquishedDeath();
+        }
+        else
+        {
+            GameObject pickedGraphic = squishGraphics[Random.Range(0, 2)];
+            AudioSource m = pickedGraphic.GetComponent<AudioSource>();
+            m.PlayOneShot(m.clip);
+        }
     }
 }
