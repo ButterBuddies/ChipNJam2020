25757eeeae84d95c887c19660222d67a4bdb6b6c
Baumdiggity <atomnomicbomb@gmail.com>
2020-05-16 22:53:41 -0700

    Termites go squiiish

---
+++ Assets/Squish.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squish : MonoBehaviour
+{
+    public GameObject[] squishGraphics;
+
+    public void Squished()
+    {
+        GameObject pickedGraphic = squishGraphics[Random.Range(0, 2)];
+        pickedGraphic.SetActive(true);
+        AudioSource m = pickedGraphic.GetComponent<AudioSource>();
+        m.PlayOneShot(m.clip);
+        pickedGraphic.transform.SetParent(null);
+        gameObject.SetActive(false);
+    }
+}
